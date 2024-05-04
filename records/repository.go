46d37c05b44893e30502a2530c470c5766c0ee2b
julianlau1998 <Julian.lau@aol.com>
2024-05-04 22:25:50 +0200

    bug-fix

--- records/repository.go
+++ records/repository.go
@@ -36,7 +36,7 @@ func (r *Repository) GetAll(limit int64, filter int64, offset int64) ([]models.R
 }
 
 func (r *Repository) Post(record *models.Record) (*models.Record, error) {
-	if record < 1 {
+	if record.Record < 1 {
 		return record, err
 	}
 	statement := `INSERT INTO records (uuid, record, username, createdDate) VALUES ($1, $2, $3, CURRENT_TIMESTAMP)`
